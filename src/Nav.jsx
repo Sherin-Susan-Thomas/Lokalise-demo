ae1304f011d35ea491aa67d7d68353fb26895af6
Sherin Susan Thomas <sherin.susan.thomas@charge-amps.com>
2022-12-19 10:15:35 +0100

    localise

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -7,7 +7,7 @@ const Nav = () => {
   return (
     <header className="nav" >
     <div > i18n React Demo </div>{" "} 
-    <div ><Link to="/" >{t('home')} </Link> <Link to="/page2 "> {t('page2')}</Link>
+    <div ><Link to="/" >{t('lblhome')} </Link> <Link to="/page2 "> {t('lblPageNumber2')}</Link>
     {" "} </div>{" "} 
     </header>
   );
